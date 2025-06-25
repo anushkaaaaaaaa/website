7ab8babc38bc8e383664cad4907e40eb20f1cc41
Anushka <anushkasharan05@gmail.com>
2025-06-25 08:15:29 +0530

    test: add e2e tests for docs and tools pages

--- cypress/pages/DocsPage.js
+++ cypress/pages/DocsPage.js
@@ -1,10 +1,25 @@
 class DocsPage {
-  visit() {
-    cy.visit('/docs');
-    // Clicking outside to remove focus (fixing hover issue)
-    cy.get('body').click(0, 0);
+  visitDocsPage() {
+    cy.get('[data-testid="NavItem-Link"][href="/docs"]').click();
+  }
+
+  goToConceptsSection() {
+    cy.get('a[href="/docs/concepts"] span').contains('Concepts').click();
+    cy.get('#concepts').should('be.visible'); //Fixes a flaky test
+  }
+
+  goToTutorialsSection() {
+    cy.get('a[href="/docs/tutorials"] span').contains('Tutorials').click();
+  }
+
+  goToToolsSection() {
+    cy.get('a[href="/docs/tools"] span').contains('Tools').click();
+  }
+
+  verifyConceptSubsection({ href, label, heading }) {
+    cy.get(`a[href="${href}"] span`).contains(label).click();
+    cy.get('h1').should('contain.text', heading);
   }
 }
-
 
 export default DocsPage;
--- cypress/pages/ToolsPage.js
+++ cypress/pages/ToolsPage.js
@@ -1,6 +1,24 @@
 class ToolsPage {
-  visit() {
+  visitToolsPage() {
     cy.visit('/tools');
+  }
+
+  verifyWebsiteLinks(href, heading) {
+    cy.get(`a[href="${href}"]`).contains('Visit Website');
+
+    cy.get('h2').should('contain.text', heading);
+  }
+
+  verifyGithubLinks(href, heading) {
+    cy.get(`a[href="${href}"]`).contains('View Github');
+
+    cy.get('h2').should('contain.text', heading);
+  }
+
+  verifyDocsLinks(href, heading) {
+    cy.get(`a[href="${href}"]`).contains('Visit Docs');
+
+    cy.get('h2').should('contain.text', heading);
   }
 }
 
